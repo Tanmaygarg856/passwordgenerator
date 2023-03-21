fa0be4d986472dd97935c2b616a6bcae482bfb01
Tanmaygarg856 <115079776+Tanmaygarg856@users.noreply.github.com>
2023-03-21 22:41:00 +0530

    paswrd generator code added

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Programiz!");
+const Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdcefghiklmnopqrstuvwxyz1234567890!@#$%^&*"
+
+function Random(item){
+let inbox  
+let No = ""
+for(let i = 0; i< item; i++)
+  {
+    inbox = Math.floor(Math.random()* Alphabet.length)
+   // No += Alphabet[inbox] 
+    No = No + Alphabet[inbox]
+  }
+  return No
+}
+
+console.log("Welcome to Programiz!"+ Random(10));
